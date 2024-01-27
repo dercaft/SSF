627bebb3aeb567cb38f064631ac26320c7f6ffd4
derek <2031754241@qq.com>
2024-01-27 03:14:55 +0000

    upd_23

--- train_scripts/vit/vtab/dtd/train_ssf.sh
+++ train_scripts/vit/vtab/dtd/train_ssf.sh
@@ -1,12 +1,13 @@
 
 source DATA_PATH.sh
-CUDA_VISIBLE_DEVICES=$1  python  -m torch.distributed.launch --nproc_per_node=1  --master_port=$2  \
-	train.py ${VTAB_PATH}/dtd  --dataset dtd --num-classes 47  --no-aug --direct-resize  --model vit_base_patch16_224_in21k  \
+CUDA_VISIBLE_DEVICES=4  python  -m torch.distributed.launch --nproc_per_node=1  --master_port=10454  \
+	train.py ${VTAB_PATH}/dtd  --dataset dtd --num-classes 47  --no-aug --direct-resize  --model vit_small_patch16_224_in21k  \
     --batch-size 128 --epochs 250 \
 	--opt adamw  --weight-decay 5e-5 \
     --warmup-lr 1e-7 --warmup-epochs 10  \
     --lr 5e-3 --min-lr 1e-8 \
     --drop-path 0 --img-size 224 \
-	--output  ${OUTPUT_PATH}/vit_base_patch16_224_in21k/vtab/dtd/ssf \
+	--output  ${OUTPUT_PATH}/vit_small_patch16_224_in21k/vtab/dtd/ssf \
 	--amp --tuning-mode ssf --pretrained  \
-	--mixup 0 --cutmix 0 --smoothing 0
+	--mixup 0 --cutmix 0 --smoothing 0 \
+	--pin-mem \
